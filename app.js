e09420c78654f94951e8d6fd390a5d28f2c44c90
diegohyenna <dgmartins87@gmail.com>
2018-06-30 09:20:00 -0300

    complete

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const fs = require('fs')
+const path = require('path')
+const ytdl = require('ytdl-core')
+const { promisify } = require('util')
+const cors = require('cors')
+
+const getInfoVideo = promisify(ytdl.getInfo)
+
+const app = express()
+
+app.use(bodyParser.urlencoded({extented: true}))
+app.use(bodyParser.json())
+app.use(express.static(path.join(__dirname, 'videos')))
+app.use(cors())
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')))
+
+app.post('/download', async (req, res) => {
+
+	try{
+
+		const info = await getInfoVideo(req.body.url.replace('https://www.youtube.com/watch?v=', ''))
+
+		ytdl(req.body.url)
+			.pipe(fs.createWriteStream(`videos/${info.title}.mp4`))
+			.on('finish', () => res.status(200).json({video: `${info.title}.mp4`}))
+	}catch(err){
+		res.status(500).json(err)
+	}
+
+})
+
+const port = process.env.PORT || 8080
+
+app.listen(port)
